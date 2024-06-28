b4ff117c33d14c0e43bc490d47241532da1aca9e
AbhishekBharati <210305105048@paruluniversity.ac.in>
2024-06-28 15:09:47 +0530

    Setup of authController is done

---
+++ Backend/Controllers/authController.js
@@ -0,0 +1,13 @@
+
+const register = async (req, res) => {
+  res.send("Logic for Registration will be stored here");
+}
+
+const login = async (req, res) => {
+  res.send("Logic for Login will be done here");
+}
+
+module.exports = {
+  register,
+  login
+}
