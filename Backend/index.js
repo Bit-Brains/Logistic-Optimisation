76fa0b1da2054ac1d99c6fb93a56536a17868c58
AbhishekBharati <210305105048@paruluniversity.ac.in>
2024-07-08 13:15:00 +0530

    Updated the routes

--- Backend/index.js
+++ Backend/index.js
@@ -1,7 +1,10 @@
 const express = require("express");
+const authRouter = require("./routes/authRoutes");
 const app = express();
 
 app.use(express.json());
+
+app.use("/auth", authRouter);
 
 app.listen(3000, () => {
   console.log(`App is listening at 3000`);
