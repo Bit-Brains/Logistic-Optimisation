f02b945de48dfc25d8bad77ad6055f25c3febb80
AbhishekBharati <210305105048@paruluniversity.ac.in>
2024-07-11 12:23:05 +0530

    Updated the routes

--- Backend/index.js
+++ Backend/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+require('dotenv').config();
 const authRouter = require("./routes/authRoutes");
 const app = express();
 
@@ -6,6 +7,7 @@ app.use(express.json());
 
 app.use("/auth", authRouter);
 
-app.listen(3000, () => {
+const port = process.env.PORT;
+app.listen(port, () => {
   console.log(`App is listening at 3000`);
 })
