f213d29126cbf0f3795e9dc9f9593482930fb31f
AbhishekBharati <210305105048@paruluniversity.ac.in>
2024-07-16 15:30:10 +0530

    Updated Index.js

--- Backend/index.js
+++ Backend/index.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 const authRouter = require("./routes/authRoutes");
 const addPartsRouter = require("./routes/addPartsRoutes");
 const inventoryRouter = require("./routes/inventoryRoutes");
+const optimizationRouter = require("./routes/optimizationRoutes");
 const app = express();
 
 app.use(express.json());
@@ -10,6 +11,7 @@ app.use(express.json());
 app.use("/auth", authRouter);
 app.use("/addItems", addPartsRouter);
 app.use("/inventory", inventoryRouter);
+app.use("/opti", optimizationRouter);
 
 const port = process.env.PORT;
 app.listen(port, () => {
